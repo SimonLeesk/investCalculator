aa015c5d4ebb406283111db101fdc93a7a7a08c0
Simon Lee <leesungkyou3@gmail.com>
2025-05-13 16:50:20 -0700

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,44 @@
-function Header() {
-  return (
-    <div id="header">
-      <img
-        src="/src/assets/investment-calculator-log.png"
-        alt="Calculator Image"
-      />
-      <h1> Investment Calculator </h1>
-    </div>
-  );
-}
+import Header from "./components/Header";
+
+const InputFields = [
+  { title: "INITIAL INVESTMENT", field: "initialInvestment" },
+  { title: "ANNUAL INVESTMENT", field: "annualInvestment" },
+  { title: "EXPECTED RETURN", field: "expectedReturn" },
+  { title: "DURATION", field: "duration" },
+];
 
 function UserInput() {
   return (
     <div id="user-input">
-      <section class="input-group">
-        <TextInput title="INITIAL INVESTMENT" field="initialInvestment" />
-        <TextInput title="ANNUAL INVESTMENT" field="annualInvestment" />
-      </section>
-      <section class="input-group">
-        <TextInput title="EXPECTED RETURN" field="expectedReturn" />
-        <div>
-          <label>DURATION</label>
-          <input type="number" name="duration" /> 
-        </div>
-      </section>
+      <InputGroup
+        label1={InputFields[0].title}
+        input1={InputFields[0].field}
+        label2={InputFields[1].title}
+        input2={InputFields[1].field}
+      />
+      <InputGroup
+        label1={InputFields[2].title}
+        input1={InputFields[2].field}
+        label2={InputFields[3].title}
+        input2={InputFields[3].field}
+      />
     </div>
   );
 }
+function InputGroup({ label1, input1, label2, input2 }) {
+  return (
+    <section class="input-group">
+      <DataInput title={label1} field={input1} />
+      <DataInput title={label2} field={input2} />
+    </section>
+  );
+}
 
-function TextInput({ title, field }) {
+function DataInput({ title, field }) {
   return (
     <div>
       <label>{title}</label>
-      <input name={field} />
+      <input type="number" name={field} />
     </div>
   );
 }
